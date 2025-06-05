304229ac6850a810acf0e1f86a7fedad5a56e3b7
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-05 15:35:04 +0700

    selesai

--- Stacklinkedlist.cpp
+++ Stacklinkedlist.cpp
@@ -16,27 +16,27 @@ public:
 class stack
 {
 private:
-    Node *top;// pointer to the top of the stack
+    Node *top;  // pointer to the top of the stack
 
 public:
     stack()
     {
-        top = NULL;//initiatelize the stack with a null top pointer
+        top = NULL; //initiatelize the stack with a null top pointer
     }
 
     int push(int value)
     {
         Node* newNode = new Node(); //1.allocate memory for new node
-        newNode->data = value;  //2.assign value to the new node
-        newNode->next = top;    //3.set the next pointer of the new node to the current top
-        top = newNode;          //4.update the top pointer to point to the new node
+        newNode->data = value;      //2.assign value to the new node
+        newNode->next = top;        //3.set the next pointer of the new node to the current top
+        top = newNode;              //4.update the top pointer to point to the new node
         cout << "push value " << value << endl;
         return value;
     }
     //isempty operation : check if step is empty
     bool isempty()
     {
-        return top == NULL;//return true if the top pointer is null
+        return top == NULL; //return true if the top pointer is null
     }
 
     //pop operation ; remove the topmost element ftom the stack
@@ -45,8 +45,8 @@ public:
         if (isempty())
         {
             cout << "stack is empty" << endl;
-            return;// if the stack is empty print a message and return
-        }
+            return; // if the stack is empty print a message and return
+        }   
         cout << "popped value:" << top->data << endl;
         top = top->next;
     }
