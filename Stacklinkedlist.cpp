cfe3a183206abf5e6261ae749799d1ce7b9f3309
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-05 15:09:03 +0700

    int main + choice

--- Stacklinkedlist.cpp
+++ Stacklinkedlist.cpp
@@ -71,4 +71,29 @@ public:
         }//return value of the top node
     }
 
+    int main()
+    {
+        stack stack;
+
+        int choice = 0;
+        int value;
+
+        while (choice != 4)
+        {
+            cout << "1.push\n";
+            cout << "2.pop\n";
+            cout << "3.peek\n";
+            cout << "4.exit\n";
+            cout << "enter your choice:";
+            cin >> choice;
+
+            switch (choice)
+            {
+            case 1:
+                cout << "enter the value to push:";
+                cin >> value;
+                stack.push(value);
+            }
+        }
+    }
 };
